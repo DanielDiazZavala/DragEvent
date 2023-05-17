e718930d0a1dd80d0cae1dcca027a9772fb9a3af
GodFranck <geraolvera644@gmail.com>
2023-05-17 03:54:37 -0600

    Se añade el juego completo

--- lib/my_game.dart
+++ lib/my_game.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+
+import 'dart:html';
 import 'package:flame/events.dart';
 import 'package:flame/experimental.dart';
 import 'package:flame/game.dart';
@@ -10,6 +12,13 @@ import 'package:flame/components.dart';
 
 import 'tap_button.dart';
 var incremento = 0;
+int puntaje=0;
+  int score=0;
+  double vel=.5;
+  double GenerarFigura=3;
+  double TiempoGenerarFigura=0;
+  int vidas=6;
+  late final Flower player;
 
 class MyGame extends FlameGame with KeyboardEvents {
   bool get debug => true;
@@ -19,7 +28,7 @@ class MyGame extends FlameGame with KeyboardEvents {
 
   @override
   Color backgroundColor() {
-    return const Color.fromARGB(255, 200, 200, 200);
+    return Color.fromARGB(255, 12, 244, 240);
   }
 
   @override
@@ -33,25 +42,28 @@ class MyGame extends FlameGame with KeyboardEvents {
   Future<void> onLoad() async {
     
     children.register<Flower>();
-    await add(Flower(
+
+    player=Flower(
       position: Vector2(size.x / 2, size.y - sizeOfPlayer.y),
       paint: Paint()..color = Colors.pink,
       size: sizeOfPlayer,
-    ));
+    );
+
+    await add(player);
      //butones de las esquinas 
-    await add(TapButton()
+  /*  await add(TapButton()
       ..position = Vector2(size.x - 50, 75)
       ..size = Vector2(100, 100));
     await add(TapButton()
       ..position = Vector2(50, 75)
       ..size = Vector2(100, 100));
-
+*/
  //definicion de tamaño de la zona
     zona = ZonaDrag(
       n: 3,
-      size: Vector2(1600,120),
-      color: const Color(0xff6ecbe5),
-      position: Vector2(0, 565),
+      size: Vector2(1600,250),
+      color: Color.fromARGB(255, 168, 133, 208),
+      position: Vector2(0, 555),
     );
     await add(zona);
     
@@ -67,9 +79,207 @@ class MyGame extends FlameGame with KeyboardEvents {
     }
      if (flower.position.x + flower.width < 0) {
       flower.position.x = size.x;
-     
     }
+    } 
+     if(vidas>0){
+
+    TiempoGenerarFigura+=dt;
+    if(TiempoGenerarFigura>GenerarFigura){
+      var PosicionRamdom=Random();
+      var   FiguraRamdom=Random();
+      var TamanoRamdom=Random();
+
+      switch( FiguraRamdom.nextInt(5)){
+        case 0:
+          add(Grillo(position: Vector2(PosicionRamdom.nextDouble()*(size.x-140),-80), size: Vector2(TamanoRamdom.nextDouble()*140,80), paint: Paint()..color = Colors.green) );
+          break;
+        case 1:
+          add(Pinguino(position: Vector2(PosicionRamdom.nextDouble()*(size.x-40),-80), size: Vector2(TamanoRamdom.nextDouble()*40,80), paint: Paint()..color = Colors.grey) );
+          break;
+        case 2:
+          add(Caballito(position: Vector2(PosicionRamdom.nextDouble()*(size.x-40),-80), size: Vector2(TamanoRamdom.nextDouble()*40,80), paint: Paint()..color = Colors.brown) );
+          break;
+        case 3:
+          add(Libreta(position: Vector2(PosicionRamdom.nextDouble()*(size.x-40),-80), size: Vector2(TamanoRamdom.nextDouble()*40,80), paint: Paint()..color = Colors.pink) );
+          break;
+        case 4:
+          add(Elefante(position: Vector2(PosicionRamdom.nextDouble()*(size.x-120),-80), size: Vector2(TamanoRamdom.nextDouble()*120,80), paint: Paint()..color = Colors.blue) );
+          break;
+      }
+      if(score%5==0){
+      vel+=.2;
+      GenerarFigura*=.9;
+      }
+
+      //add(Pinguino(position: Vector2(PosicionRamdom.nextDouble()*(size.x-40),-80), size: Vector2(40,80), paint: Paint()..color = Colors.pink) );
+      TiempoGenerarFigura=0;
     }
+
+    bool GriloColision(Flower player, Grillo figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    bool PinguinoColision(Flower player, Pinguino figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    bool CaballoColision(Flower player, Caballito figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    bool StickmanColision(Flower player, Stickman figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    bool BallenaColision(Flower player, Ballena figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    bool LibretaColision(Flower player, Libreta figura) {
+      Rect rectPlayer = Rect.fromLTWH(
+          player.position.x, player.position.y, player.size.x, player.size.y);
+
+      Rect rectFigura = Rect.fromLTWH(
+          figura.position.x, figura.position.y, figura.size.x, figura.size.y);
+
+      return rectPlayer.overlaps(rectFigura);
+    }
+
+    for(var text in children.query<TextComponent>()){
+      children.remove(text);
+    }
+    add(TextComponent(
+         priority: 100,
+         text: score.toString(),
+         position: Vector2(850, 80),
+         size: Vector2(350, 300),
+       ),);
+    
+    add(TextComponent(
+         priority: 100,
+         text: "Vidas:"+vidas.toString(),
+         position: Vector2(850, 50),
+         size: Vector2(150, 100),
+       ));
+
+    for (var figura in children.query<Grillo>()) {
+      if (GriloColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+      }
+    }
+
+    for (var figura in children.query<Pinguino>()) {
+      if (PinguinoColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+      }
+    }
+
+    for (var figura in children.query<Caballito>()) {
+      if (CaballoColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+   }
+    }
+
+    for (var figura in children.query<Ballena>()) {
+      if (BallenaColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+      }
+    }
+
+    for (var figura in children.query<Libreta>()) {
+      if (LibretaColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+      }
+    }
+
+    for (var figura in children.query<Stickman>()) {
+      if (StickmanColision(player, figura)) {
+        children.remove(figura);
+        score += 1;
+        print(score);
+      } else {
+        figura.y += vel;
+        if (figura.y > size.y) {
+          children.remove(figura);
+          vidas--;
+        }
+      }
+    }
+
+    }
+
+
+
 
     super.update(dt);
   }
@@ -133,11 +343,11 @@ class ZonaDrag extends PositionComponent with DragCallbacks {
   final Color color;
   final Paint colorRectangulo = Paint();
   final Paint colorBorde = Paint()
-    ..color = const Color(0xFFffffff)
+    ..color = Color.fromARGB(255, 197, 41, 41)
     ..style = PaintingStyle.stroke
     ..strokeWidth = 3;
   final _shadowPaint = Paint()
-    ..color = const Color(0xFF000000)
+    ..color = Color.fromARGB(255, 205, 32, 32)
     ..maskFilter = const MaskFilter.blur(BlurStyle.normal, 4.0);
   late final Path zonaRectangulo;
   bool _isDragged = false;
@@ -150,7 +360,7 @@ class ZonaDrag extends PositionComponent with DragCallbacks {
   @override
   void render(Canvas canvas) {
    
-      colorRectangulo.color = color.withOpacity(0.1);
+      colorRectangulo.color = color.withOpacity(0.4);
       canvas.drawPath(zonaRectangulo, colorRectangulo);
       canvas.drawPath(zonaRectangulo, colorBorde);
   }
@@ -174,9 +384,9 @@ class ZonaDrag extends PositionComponent with DragCallbacks {
     final delta = event.delta;
    
     if ((delta.x > 0)) {
-      incremento+= 3;
+      incremento+= 10;
     } else if(delta.x < 0) {
-      incremento-= 3;
+      incremento-= 10;
     }
   }
 
